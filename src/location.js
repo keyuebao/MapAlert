1dfd2da6d303a9e4dee0dce3c39c2606d44a673f
Keyue Bao <keyuebao@users.noreply.github.com>
2015-09-16 17:53:38 +0800

    Initial Commit

---
+++ src/location.js
@@ -0,0 +1,28 @@
+function locationSuccess(pos) {
+}
+
+function locationError(err) {
+  console.log('Error requesting location, check your settings!');
+}
+
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(
+    locationSuccess,
+    locationError,
+    {timeout: 15000, maximumAge: 60000}
+  );
+}
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
+  function(e) {
+    console.log('PebbleKit JS ready!');
+  }
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+    console.log('AppMessage received!');
+  }                     
+);
